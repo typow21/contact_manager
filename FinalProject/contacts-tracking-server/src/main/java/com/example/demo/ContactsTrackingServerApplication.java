9a78691aa1649b178a3b8e31e1a112d7c4a22963
dirtdiver2091 <dirtdiver2091@DESKTOP-95U3MLA.localdomain>
2020-04-14 17:37:14 -0400

    Spring server created

---
+++ FinalProject/contacts-tracking-server/src/main/java/com/example/demo/ContactsTrackingServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsTrackingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsTrackingServerApplication.class, args);
+	}
+
+}
+
+//THERE IS NOTHING ELSE TO DO IN THIS CLASS, ALL IT DOES IS START THE SERVER UP.
+//TO START THE SERVER UP, GO INTO THIS CLASS AND HIT RUN. THE SERVER WILL START AND YOU
+//WILL BE ABLE TO GO TO THE WEBSITE AND SEE IF IT IS UP AND CAN CHECK IF EVERYTHING IS WORKING.
