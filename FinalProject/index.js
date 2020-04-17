88d3794df35779186c50cb2bde026f4b56e382aa
Tyler <tuh26432@temple.edu>
2020-04-17 00:28:00 -0400

    Overall redesign

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -1,86 +1,68 @@
+let contacts = []
 
-const data = [{
-    FirstName : "Tyler",
-    LastName : "Powell",
-    phonenumber: "570-460-0704",
-    address : "2029 N Broad st."
-}, {
-    FirstName : "And",
-    LastName : "Pij",
-    phonenumber: "215-548-3587",
-    address : "2029 N Broad st."
-},{
-    FirstName : "And",
-    LastName : "Pij",
-    phonenumber: "215-548-3587",
-    address : "2029 N Broad st."
-},{
-    FirstName : "And",
-    LastName : "Pij",
-    phonenumber: "215-548-3587",
-    address : "2029 N Broad st."
-},{
-    FirstName : "Tyler",
-    LastName : "Powell",
-    phonenumber: "570-460-0704",
-    address : "2029 N Broad st."
-}, {
-    FirstName : "And",
-    LastName : "Pij",
-    phonenumber: "215-548-3587",
-    address : "2029 N Broad st."
-},{
-    FirstName : "And",
-    LastName : "Pij",
-    phonenumber: "215-548-3587",
-    address : "2029 N Broad st."
-},{
-    FirstName : "And",
-    LastName : "Pij",
-    phonenumber: "215-548-3587",
-    address : "2029 N Broad st."
-}]
-
+loadContacts();
 function loadPage(){
-    for (var cardInd in data){
+    for (var cardInd in contacts){
         var cardDiv = document.createElement('div');
         cardDiv.setAttribute('class', 'card');
         cardDiv.setAttribute('id', 'card'+cardInd);
+        cardDiv.setAttribute('name', contacts[cardInd].id + '')
         document.getElementById("grid-container").appendChild(cardDiv);
-        cardDiv.onclick = cardLink;
-        // console.log()
+        cardDiv.setAttribute('onclick', 'cardLink('+contacts[cardInd].id+')')
+        
 
         var name = document.createElement('p');
         name.setAttribute('class', 'name');
         name.setAttribute('id', 'name'+cardInd);
         document.getElementById('card'+cardInd).appendChild(name);
+        
+        var img = document.createElement('img');
+        img.setAttribute('src', 'profile.png');
+        img.setAttribute('alt','Profile picture');
+        document.getElementById('card'+cardInd).appendChild(img);
 
-        var phonenumber = document.createElement('p');
-        phonenumber.setAttribute('id', 'phonenumber'+cardInd);
-        phonenumber.setAttribute('class', 'name');
-        document.getElementById('card'+cardInd).appendChild(phonenumber);
+        // var phonenumber = document.createElement('p');
+        // phonenumber.setAttribute('id', 'phonenumber'+cardInd);
+        // phonenumber.setAttribute('class', 'name');
+        // document.getElementById('card'+cardInd).appendChild(phonenumber);
 
-        var address = document.createElement('p');
-        address.setAttribute('id', 'address'+cardInd);
-        address.setAttribute('class', 'name');
-        document.getElementById('card'+cardInd).appendChild(address);
+        // var address = document.createElement('p');
+        // address.setAttribute('id', 'address'+cardInd);
+        // address.setAttribute('class', 'name');
+        // document.getElementById('card'+cardInd).appendChild(address);
 
-        document.getElementById("name"+cardInd).innerHTML = data[cardInd].FirstName + " " + data[cardInd].LastName;
-        document.getElementById("phonenumber"+cardInd).innerHTML = data[cardInd].phonenumber;
-        document.getElementById("address"+ cardInd).innerHTML = data[cardInd].address;
+        // var relationship = document.createElement('p');
+        // relationship.setAttribute('id', 'relationship'+cardInd);
+        // relationship.setAttribute('class', 'name');
+        // document.getElementById('card'+cardInd).appendChild(relationship);
+
+        document.getElementById("name"+cardInd).innerHTML = contacts[cardInd].firstName + " " + contacts[cardInd].lastName;
+        // document.getElementById("phonenumber"+cardInd).innerHTML = contacts[cardInd].phoneNumber;
+        // document.getElementById("address"+ cardInd).innerHTML = contacts[cardInd].address;
     }
 }
-loadPage();
 
-function cardLink(){
-    window.location.href = "./newcontact.html";
+function cardLink(id){
+    window.location.href = "./contactcard.html?id="+id+"";
+    test(id);
+}
+
+function test(cardInd){
+    console.log(cardInd);
+}
+
+async function loadContacts(){
+    const resp =  await fetch("http://127.0.0.1:8080/load-contacts");
+    contacts = await resp.json();
+    console.log(contacts);
+    loadPage();
 }
 
 // function createCard(index){
-//     console.log(data);
-//     document.getElementById("name"+index).innerHTML = data[index].FirstName + " " + data[index].LastName;
-//     document.getElementById("number"+index).innerHTML = data[index].phone;
-//     document.getElementById("address"+ index).innerHTML = data[index].address;
+//     console.log(contacts);
+//     document.getElementById("name"+index).innerHTML = contacts[index].FirstName + " " + contacts[index].LastName;
+//     document.getElementById("number"+index).innerHTML = contacts[index].phone;
+//     document.getElementById("address"+ index).innerHTML = contacts[index].address;
 // }
 
 // createCard(0);
@@ -88,7 +70,7 @@ function cardLink(){
 // createCard(0);
 // createCard(1);
 
-// console.log(data.FirstName);
+// console.log(contacts.FirstName);
 
 // fetch("http://127.0.0.1:8080/test/")
 //     .then((response) => {
