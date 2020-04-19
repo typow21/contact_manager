fa726b4d1f3a9fc6cb187990c21d120886267a45
Tyler <tuh26432@temple.edu>
2020-04-18 22:44:49 -0400

    Relations added and working, need to fix editing

--- FinalProject/Front-end/contactcard.js
+++ FinalProject/Front-end/contactcard.js
@@ -12,6 +12,25 @@ async function fetchCard(){
     document.getElementById("name").innerHTML = contact.firstName + " " + contacts[id].lastName;
     document.getElementById("phoneNumber").innerHTML = contact.phoneNumber;
     document.getElementById("address").innerHTML = contact.address;
+    relationships = contact.relationships;
+    console.log(relationships)
+    if(relationships[0] != -1){
+        console.log(contacts[relationships[0]].firstName )
+        document.getElementById('mother').innerHTML += contacts[relationships[0]].firstName + " "+ contacts[relationships[0]].lastName
+        document.getElementById('mother').setAttribute('href',"./contactcard.html?id="+contacts[relationships[0]].id+"")
+    }
+    if(relationships[1] != -1){
+        document.getElementById('father').innerHTML += contacts[relationships[1]].firstName + " " + contacts[relationships[1]].lastName
+        document.getElementById('father').setAttribute('href',"./contactcard.html?id="+contacts[relationships[1]].id+"")
+    }
+    if(relationships[2] != -1){
+        document.getElementById('brother').innerHTML += contacts[relationships[2]].firstName + " " + contacts[relationships[2]].lastName
+        document.getElementById('brother').setAttribute('href',"./contactcard.html?id="+contacts[relationships[2]].id+"")
+    }
+    if(relationships[3] != -1){
+        document.getElementById('sister').innerHTML += contacts[relationships[3]].firstName + " " + contacts[relationships[3]].lastName
+        document.getElementById('sister').setAttribute('href',"./contactcard.html?id="+contacts[relationships[3]].id+"")
+    }
     document.getElementById("edit").setAttribute('onclick', 'cardLink('+id+')')
 }
 fetchCard();
--- FinalProject/Front-end/edit-contact.js
+++ FinalProject/Front-end/edit-contact.js
@@ -9,6 +9,7 @@ async function fetchCard(){
     console.log(contacts);
     console.log(contacts[id])
     contact= contacts[id]
+    relationships = contact.relationships;
     prefillInfo();
     // document.getElementById("name").innerHTML = contact.firstName + " " + contacts[id].lastName;
     // document.getElementById("phoneNumber").innerHTML = contact.phoneNumber;
@@ -18,6 +19,7 @@ async function fetchCard(){
 fetchCard();
 
 function prefillInfo(){
+    addRelations();
     document.getElementById("firstName").setAttribute('value', contacts[id].firstName);
     document.getElementById("lastName").setAttribute('value', contacts[id].lastName);
     document.getElementById("phoneNumber").setAttribute('value', contacts[id].phoneNumber);
@@ -25,6 +27,14 @@ function prefillInfo(){
 }
 
 async function editContact(){
+    var mother = document.getElementById("mother").value
+    console.log(mother)
+    var father = document.getElementById("father").value
+    console.log(father)
+    var brother = document.getElementById("brother").value
+    console.log(brother)
+    var sister = document.getElementById('sister').value
+    console.log(sister)
     console.log("reached")
     const resp = await fetch("http://127.0.0.1:8080/edit-contact",{
         method:"POST",
@@ -32,7 +42,8 @@ async function editContact(){
                                 firstName: document.getElementById("firstName").value,
                                 lastName : document.getElementById("lastName").value, 
                                 phoneNumber : document.getElementById("phoneNumber").value,
-                                address : document.getElementById("address").value
+                                address : document.getElementById("address").value,
+                                relationships : [mother, father, brother, sister]
                                 // relationships : {"Mother": 1, "Father": 2}
                                 // relationships : document.getElementById("relationships").value
         }),
@@ -42,4 +53,43 @@ async function editContact(){
     // const newContact = await resp.json();
     // JSON.parse(resp)
     window.location.href = "./index.html";
+}
+
+function addRelations(){
+
+    for(contact in contacts){
+        console.log(contact)
+        var motherOption = document.createElement('option')
+        motherOption.setAttribute('value', contacts[contact].id);
+        motherOption.innerHTML = contacts[contact].firstName+ " " + contacts[contact].lastName;
+        console.log(document.getElementById('mother'))
+        document.getElementById('mother').append(motherOption)
+
+        var fatherOption = document.createElement('option')
+        fatherOption.setAttribute('value', contacts[contact].id);
+        fatherOption.innerHTML = contacts[contact].firstName+ " " + contacts[contact].lastName;
+        console.log(document.getElementById('father'))
+        document.getElementById('father').append(fatherOption)
+
+        var brotherOption = document.createElement('option')
+        brotherOption.setAttribute('value', contacts[contact].id);
+        brotherOption.innerHTML = contacts[contact].firstName+ " " + contacts[contact].lastName;
+        console.log(document.getElementById('brother'))
+        document.getElementById('brother').append(brotherOption)
+
+        var sisterOption = document.createElement('option')
+        sisterOption.setAttribute('value', contacts[contact].id);
+        sisterOption.innerHTML = contacts[contact].firstName+ " " + contacts[contact].lastName;
+        console.log(document.getElementById('sister'))
+        document.getElementById('sister').append(sisterOption)
+    }
+    document.getElementById('mother').value = relationships[0];
+    document.getElementById('father').value = relationships[1];
+    document.getElementById('brother').value = relationships[2];
+    document.getElementById('sister').value = relationships[3];
+    // var relationInput = document.createElement('input');
+    // relationInput.setAttribute('type', 'text');
+    // relationInput.setAttribute('id', 'relationsInput');
+    // relationInput.setAttribute('name', 'relationsInput');
+    // relationSelect32.append(relationInput);
 }
